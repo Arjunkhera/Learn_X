3abf2d724edfe759271df02024116391e31a0484
Arjun Khera <personalarjunk@gmail.com>
2021-04-01 17:30:33 +0530

    React Maximilian Chapter 8 Burger Builder Build Control Screen

--- LearnWebDevelopment/Websites/ReactCompleteGuideMaxmillan/burger-builder/src/Containers/BurgerBuilder/BurgerBuilder.js
+++ LearnWebDevelopment/Websites/ReactCompleteGuideMaxmillan/burger-builder/src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Aux from '../../HOC/Auxilliary';
 import Burger from '../../Components/Burger/Burger';
+import BuildControls from '../../Components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
-            salad: 1,
-            bacon: 1,
-            cheese: 2,
+            salad: 0,
+            bacon: 0,
+            cheese: 1,
             meat: 2,
         }
     }
@@ -16,7 +17,7 @@ class BurgerBuilder extends Component {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
-                <div>Build Controls</div>
+                <BuildControls/>
             </Aux>
         );
     }
