653c6a5466c84e36cd61219f4108e77643956c24
Arjun Khera <personalarjunk@gmail.com>
2021-04-26 13:11:42 +0530

    Interim Commit

--- LearnJava/learn/src/main/java/com/arjun/learn/collections/Iteration.java
+++ LearnJava/learn/src/main/java/com/arjun/learn/collections/Iteration.java
@@ -1,38 +1,32 @@
-package com.arjun.collections;
+package com.arjun.learn.collections;
 
 import java.util.*;
 
 public class Iteration {
     public static void main(String[] args) {
-        // introduction();
-        // usingIterator();
+        iterator();
+        System.out.println("------------");
+
+        enhancedForLoop();
+        System.out.println("------------");
+
         listIterator();
+        System.out.println("------------");
     }
 
-    public static void introduction() {
-        Iterable<Integer> iterable = List.of(1,2,3);
+    public static void iterator() {
+        Iterator<Integer> iteratorOnList = List.of(1,2,3,4,5).iterator();
 
-        // use for each loop
-        for(Integer integer : iterable) {
-            System.out.println(integer);
-        }
-        // use for each method
-        iterable.forEach(System.out::println);
+        while(iteratorOnList.hasNext())
+            System.out.printf("%d ", iteratorOnList.next());
+        System.out.println("");
 
-        // output order is not guaranteed in sets
-        iterable = Set.of(1,2,3);
-        iterable.forEach(System.out::println);
-    }
-
-    public static void usingIterator() {
         Set<Long> set = new TreeSet<>();
         set.add(10L);
         set.add(5L);
         set.add(18L);
         set.add(14L);
         set.add(9L);
-
-        System.out.println(set);
 
         Iterator<Long> iter = set.iterator();
         while (iter.hasNext()) {
@@ -42,25 +36,72 @@ public class Iteration {
             }
         }
 
-        System.out.println(set);
+        for(Long lg: set)
+            System.out.printf("%d ", lg);
+        System.out.println("");
     }
 
+    public static void enhancedForLoop() {
+        Iterable<Integer> iterable = List.of(1,2,3);
+
+        // use for each loop
+        for(Integer integer : iterable)
+            System.out.printf("%d ", integer);
+        System.out.println("");
+
+        // use for each method
+        iterable.forEach(System.out::println);
+
+        // output order is not guaranteed in sets
+        iterable = Set.of(1,2,3);
+        iterable.forEach(System.out::println);
+    }
+
+    /**
+     * ListIterator introduction
+     */
     public static void listIterator() {
         List<Integer> list = List.of(1, 2, 3, 4);
         ListIterator<Integer> iterator = list.listIterator(); // only for lists!
 
-        // go to the last element
+        // We can go forward
         while (iterator.hasNext()) {
-            System.out.println(iterator.next());
+            System.out.printf("Forward Index: %d Value: %d\n", iterator.nextIndex(), iterator.next());
         }
-        // another way is to directly use the index
-        iterator = list.listIterator(list.size());
 
-        // print elements in the backward order with their indexes
+        // We can go backward
+        iterator = list.listIterator(list.size());
         while (iterator.hasPrevious()) {
-            int previousIndex = iterator.previousIndex();
-            int element = iterator.previous();
-            System.out.println(element + " on " + previousIndex);
+            System.out.printf("Backward Index: %d Value: %d\n", iterator.previousIndex(), iterator.previous());
         }
+
+        System.out.println("--------------------");
+        // Optional Methods : will throw UnsupportedOperationException if not implemented
+        List<Integer> arrayList = new ArrayList<Integer>(List.of(1,2,3,4));
+
+        ListIterator<Integer> addingElement = arrayList.listIterator();
+        // Add element
+        addingElement.add(5); // will add 5 before 1
+        printList(arrayList);
+
+        ListIterator<Integer> removingElement = arrayList.listIterator();
+        removingElement.next();   // will return 5
+        removingElement.next();   // will return 1
+        removingElement.remove(); // will remove 1
+        printList(arrayList);
+
+        ListIterator<Integer> setElement = arrayList.listIterator();
+        setElement.next();  // will return 5
+        setElement.set(10); // will change 5 to 10
+        printList(arrayList);
+    }
+
+    public static void printList(List<Integer> list) {
+        ListIterator<Integer> iterator = list.listIterator();
+
+        while (iterator.hasNext()) {
+            System.out.printf("Index: %d Value: %d\n", iterator.nextIndex(), iterator.next());
+        }
+        System.out.println("--------------------");
     }
 }
